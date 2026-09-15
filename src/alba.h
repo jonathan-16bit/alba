e05ee0dc1c6f6294359fb37fa949fcf2e2e1cd44
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-15 17:54:50 +0530

    feat: add Position and Movelist definitions

--- src/alba.h
+++ src/alba.h
@@ -7,4 +7,19 @@
 
 #define START_POSITION "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 "
 
+// Snapshot of current board state
+typedef struct {
+  int board[128];  // 0x88 board rep
+  int king_square[2];
+  int side;  // Side to move next
+  int enpassant;
+  int castle;
+} Position;
+
+// List of generated moves
+typedef struct {
+  int moves[256];
+  int count;  // Of moves in the array
+} Movelist;
+
 #endif
