0d206e532ed33d2a01fa7b50bb3741fbdeb3b306
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-15 17:40:29 +0530

    feat: add include guard

--- src/alba.h
+++ src/alba.h
@@ -1,5 +1,10 @@
+#ifndef ALBA_H
+#define ALBA_H
+
 #include <string.h>
 #include <sys/time.h>
 #include <sys/select.h>
 
 #define START_POSITION "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 "
+
+#endif
