8b5886aa1f6636b268c7086e94e81e77e422922d
jonathan-16bit <207332460+jonathan-16bit@users.noreply.github.com>
2026-09-18 16:10:08 +0530

    feat: add castling rights bitmask array

--- src/defs.c
+++ src/defs.c
@@ -6,3 +6,15 @@ int king_square[2] = {E1, E8};
 int side =  WHITE;
 int enpassant = NONE;
 int castle = 15;
+
+// Castling rights masks
+int castling_rights[128] = {
+     7, 15, 15, 15,  3, 15, 15, 11,  NONE, NONE, NONE, NONE, NONE, NONE, NONE, NONE,
+    15, 15, 15, 15, 15, 15, 15, 15,  NONE, NONE, NONE, NONE, NONE, NONE, NONE, NONE,
+    15, 15, 15, 15, 15, 15, 15, 15,  NONE, NONE, NONE, NONE, NONE, NONE, NONE, NONE,
+    15, 15, 15, 15, 15, 15, 15, 15,  NONE, NONE, NONE, NONE, NONE, NONE, NONE, NONE,
+    15, 15, 15, 15, 15, 15, 15, 15,  NONE, NONE, NONE, NONE, NONE, NONE, NONE, NONE,
+    15, 15, 15, 15, 15, 15, 15, 15,  NONE, NONE, NONE, NONE, NONE, NONE, NONE, NONE,
+    15, 15, 15, 15, 15, 15, 15, 15,  NONE, NONE, NONE, NONE, NONE, NONE, NONE, NONE,
+    13, 15, 15, 15, 12, 15, 15, 14,  NONE, NONE, NONE, NONE, NONE, NONE, NONE, NONE
+};
